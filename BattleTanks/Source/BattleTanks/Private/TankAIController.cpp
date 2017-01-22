c9bf010a2d083f6f76ecc6850604f2ab2d0a6f0b
Alexandre Anseline <alexandre.anseline@yahoo.com>
2017-01-22 16:01:06 +0400

    + Finding the player controlled tank

--- BattleTanks/Source/BattleTanks/Private/TankAIController.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankAIController.cpp
@@ -2,6 +2,7 @@
 
 #include "BattleTanks.h"
 #include "TankAIController.h"
+#include "../Public/TankAIController.h"
 
 
 
@@ -15,8 +16,23 @@ void ATankAIController::BeginPlay() {
 	}else{
 		UE_LOG(LogTemp, Warning, TEXT("AI %s successfully spawned"), *ControlledTank->GetName());
 	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("No Player controlled Tanks"));
+	}else {
+		UE_LOG(LogTemp, Warning, TEXT("Player controlled Tanks Found %s"), *PlayerTank->GetName());
+	}
 }
 
 ATank *ATankAIController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }
